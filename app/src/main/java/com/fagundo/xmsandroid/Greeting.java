6155fa059059778f5e519964c04990c97e9b04be
Nathan Fagundo <nathan@fagundo.com>
2015-12-22 18:27:33 -0500

    Initial sync

---
+++ app/src/main/java/com/fagundo/xmsandroid/Greeting.java
@@ -0,0 +1,22 @@
+package com.fagundo.xmsandroid;
+
+public class Greeting {
+
+        private String xms;
+        private String username;
+        private String password;
+
+        public String getXms() {
+            return this.xms;
+        }
+
+        public String getUsername() {
+            return this.username;
+        }
+
+        public String getPassword() {
+            return this.password;
+        }
+
+}
+
